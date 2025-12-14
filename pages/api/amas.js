1a56d14164b6c2c63c15aed7604f11e583ec407c
Doğan Can Bakır <dogancanbakir@protonmail.com>
2025-12-14 21:41:01 +0300

    get past+upcoming events

--- pages/api/amas.js
+++ pages/api/amas.js
@@ -3,7 +3,6 @@ import { filter } from 'lodash'
 
 export default async (req, res) => {
   let events = await getEvents()
-  // events = filter(events, { ama: true })
-  events = filter(events, (event) => event.title.startsWith('AMA:'))
+  events = filter(events, { ama: true })
   res.json(events)
 }
